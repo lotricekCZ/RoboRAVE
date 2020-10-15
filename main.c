526f82d17cf1b4deb3ec8ef46f69a67d43adfd0b
Jakub Ramašeuski <jakub@skaryna.net>
2020-10-15 22:30:49 +0200

    changed case sensitive stuff

--- main.c
+++ main.c
@@ -12,52 +12,54 @@
 #define r_wheel 0.044f // in meters
 #define pi 3.141592653589793238462643383279502884L
 #define dist_wheel 0.125f // in meters
-#define DECIMAL float
-#define SIGNED int16_t
+#define decimal float
+#define signed int16_t
 
-const DECIMAL omega_wheel_const = (pi * r_wheel) / (N_wheel * dist_wheel);
+const decimal omega_wheel_const = (pi * r_wheel) / (N_wheel * dist_wheel);
+const decimal wheel_step_lenght_const = (2 * pi * r_wheel) / (N_wheel);
 
-DECIMAL calculate_omega(DECIMAL T_avg = 0){
+decimal calculate_omega(decimal T_avg = 0){
 	//~ printf("omega con %f\nTavg %f\n %.20Lf\n", omega_wheel_const, T_avg, pi);
 	return (omega_wheel_const / T_avg);
 	}
 
-DECIMAL calculate_T_avg(SIGNED T_left = 0, SIGNED T_right = 0){
-	
-	return ((T_right - T_left) / (DECIMAL)(T_right * T_left));
-	}
-	
-DECIMAL c_f(char * input){
-	return (DECIMAL)atof(input);
+decimal calculate_T_avg(signed T_left = 0, signed T_right = 0){
+	return ((T_right - T_left) / (decimal)(T_right * T_left));
 	}
 
-SIGNED c_i(char * input){
+decimal
+
+decimal c_f(char * input){
+	return (decimal)atof(input);
+	}
+
+signed c_i(char * input){
 	return atoi(input);
 	}
 
-DECIMAL get_gamma(SIGNED x_rel, SIGNED y_rel){
-	return atan((DECIMAL)y_rel / (DECIMAL)x_rel);
+decimal get_gamma(signed x_rel, signed y_rel){
+	return atan((decimal)y_rel / (decimal)x_rel);
 	}
 	
-DECIMAL get_radius(SIGNED x_rel, SIGNED y_rel){
+decimal get_radius(signed x_rel, signed y_rel){
 	return sqrt(pow(x_rel, 2) + pow(y_rel, 2));
 	}
 
 
-DECIMAL coords_x(DECIMAL alpha, DECIMAL gamma, DECIMAL radius, uint8_t dec){
+decimal coords_x(decimal alpha, decimal gamma, decimal radius, uint8_t dec){
 	return -1*(((dec >> 1) & 1? 1 : -1))*cos((alpha) + ((dec & 1)? (1) : (-1))* gamma)*radius;
 	}
 
-DECIMAL coords_y(DECIMAL alpha, DECIMAL gamma, DECIMAL radius, uint8_t dec){
+decimal coords_y(decimal alpha, decimal gamma, decimal radius, uint8_t dec){
 	return -1*(((dec >> 1) & 1? 1 : -1))*sin((alpha) + ((dec & 1)? (1) : (-1))* gamma)*radius;
 	}
 
-void get_coords(DECIMAL x_rel, DECIMAL y_rel, DECIMAL x, DECIMAL y, DECIMAL alpha){
+void get_coords(decimal x_rel, decimal y_rel, decimal x, decimal y, decimal alpha){
 	auto var = std::async(get_gamma, x_rel, y_rel);
 	auto var1 = std::async(get_radius, x_rel, y_rel);
-	DECIMAL ralpha = alpha * pi / 180.0f;
-	DECIMAL gamma = var.get();
-	DECIMAL radius = var1.get();
+	decimal ralpha = alpha * pi / 180.0f;
+	decimal gamma = var.get();
+	decimal radius = var1.get();
 	auto var2 = std::async(coords_x, ralpha, gamma, radius, (uint8_t) ((x_rel > 0) | (((x_rel < 0) & (y_rel > 0)) | ((x_rel > 0) && (y_rel < 0))) ));
 	auto var3 = std::async(coords_y, ralpha, gamma, radius, (uint8_t)((x_rel > 0) | (((x_rel < 0) & (y_rel > 0)) | ((x_rel > 0) && (y_rel < 0)))));
 	
@@ -65,13 +67,12 @@ void get_coords(DECIMAL x_rel, DECIMAL y_rel, DECIMAL x, DECIMAL y, DECIMAL alph
 	}
 
 
-
 int main(int argc, char *argv[])
 {
 
 	switch((char)argv[1][0]){
 		case '1':
-			printf("result %.20f", (DECIMAL)calculate_omega(c_f(argv[2])));
+			printf("result %.20f", (decimal)calculate_omega(c_f(argv[2])));
 			break;
 			
 		case '2':
