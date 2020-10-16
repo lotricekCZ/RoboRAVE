a7c62b94f481dea83a4f60e32c0c6775d62241c7
Jakub Ramašeuski <jakub@skaryna.net>
2020-10-16 15:51:34 +0200

    adding tank class for steering

--- main.c
+++ main.c
@@ -18,6 +18,8 @@
 const decimal omega_wheel_const = (pi * r_wheel) / (N_wheel * dist_wheel);
 const decimal wheel_step_lenght_const = (2 * pi * r_wheel) / (N_wheel);
 
+#include "./tank/tank.h"
+
 decimal calculate_omega(decimal T_avg = 0){
 	//~ printf("omega con %f\nTavg %f\n %.20Lf\n", omega_wheel_const, T_avg, pi);
 	return (omega_wheel_const / T_avg);
@@ -27,7 +29,9 @@ decimal calculate_T_avg(signed T_left = 0, signed T_right = 0){
 	return ((T_right - T_left) / (decimal)(T_right * T_left));
 	}
 
-decimal
+decimal calculate_distance_wheel(signed frequency, decimal time){
+	return abs(frequency * time * wheel_step_lenght_const);
+	}
 
 decimal c_f(char * input){
 	return (decimal)atof(input);
@@ -66,25 +70,29 @@ void get_coords(decimal x_rel, decimal y_rel, decimal x, decimal y, decimal alph
 	printf("result [%f; %f]\n",x + var2.get(), y + var3.get());	
 	}
 
-
 int main(int argc, char *argv[])
 {
 
 	switch((char)argv[1][0]){
 		case '1':
-			printf("result %.20f", (decimal)calculate_omega(c_f(argv[2])));
+			printf("result %.10f", (decimal)calculate_omega(c_f(argv[2])));
 			break;
 			
 		case '2':
-			printf("result %.20f", calculate_T_avg(c_i(argv[2]), c_i(argv[3])));
+			printf("result %.10f", calculate_T_avg(c_i(argv[2]), c_i(argv[3])));
 			break;
 			
 		case '3':
-			printf("result %.20f", get_radius(c_i(argv[2]), c_i(argv[3])));
+			printf("result %.10f", get_radius(c_i(argv[2]), c_i(argv[3])));
 			break;
 		
 		case '4':
-			get_coords(c_f(argv[2]), c_f(argv[3]), c_f(argv[4]), c_f(argv[5]), c_f(argv[6]));
+			auto var = std::async(c_f, argv[2]);
+			auto var1 = std::async(c_f, argv[3]);
+			auto var2 = std::async(c_f, argv[4]);
+			auto var3 = std::async(c_f, argv[5]);
+			auto var4 = std::async(c_f, argv[6]);
+			get_coords(var.get(), var1.get(), var2.get(), var3.get(), var4.get());
 			break;
 		}
 	return 0;
