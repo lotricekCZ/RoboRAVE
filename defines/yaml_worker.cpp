2519abf92d54b76ac307fb11b5c60a316c7f5680
Jakub Ramašeuski <jakub@skaryna.net>
2021-03-02 23:16:50 +0100

    Now it reads data from config.yaml, guess that yaml is the way.

--- defines/yaml_worker.cpp
+++ defines/yaml_worker.cpp
@@ -32,11 +32,29 @@ void load_config(){
 	//~ YAML::Node config = YAML::Load("{candle: 3.14159, [0, 1]: integers}");
 	//~ YAML::Node node = YAML::Load("{pi: 3.14159, [0, 1]: integers}");
 
-// this needs the conversion from Node to double
-	double pi = config["tresholds"]["mAP"]["candle"].as<double>();
+	tresholds::mAP::candle = 					config["tresholds"]["mAP"]["candle"].as<decimal_n>();
+	tresholds::mAP::barrier = 					config["tresholds"]["mAP"]["barrier"].as<decimal_n>();
+	tresholds::mAP::fire = 						config["tresholds"]["mAP"]["fire"].as<decimal_n>();
+	tresholds::mAP::line_surround = 			config["tresholds"]["mAP"]["line_surround"].as<decimal_n>();
+	tresholds::mAP::line_border = 				config["tresholds"]["mAP"]["line_border"].as<decimal_n>();
+	
+	tresholds::explo::objects::barrier = 		config["priority"]["exploration"]["barrier"].as<decimal_n>();
+	tresholds::explo::objects::candle = 		config["priority"]["exploration"]["candle"].as<decimal_n>();
+	tresholds::explo::objects::unknown = 		config["priority"]["exploration"]["unknown"].as<decimal_n>();
+	tresholds::explo::objects::candle_blown = 	config["priority"]["exploration"]["candle_blown"].as<decimal_n>();
+	tresholds::explo::objects::discovered = 	config["priority"]["exploration"]["discovered"].as<decimal_n>();
+	tresholds::explo::objects::interesting = 	config["priority"]["exploration"]["interesting"].as<decimal_n>();
+	tresholds::explo::objects::boring = 		config["priority"]["exploration"]["boring"].as<decimal_n>();
 	//~ YAML::Node ch = config["thresholds"];
 	//~ YAML::Node ar = ch["mAP"];
-	std::cout << pi << std::endl;
+	std::cout << tresholds::explo::objects::barrier 		 << std::endl;
+	std::cout << tresholds::explo::objects::candle 		 	<< std::endl;
+	std::cout << tresholds::explo::objects::unknown 		 << std::endl;
+	std::cout << tresholds::explo::objects::candle_blown 	 << std::endl;
+	std::cout << tresholds::explo::objects::discovered 	 	<< std::endl;
+	std::cout << tresholds::explo::objects::interesting 	 << std::endl;
+	std::cout << tresholds::explo::objects::boring 		 	<< std::endl;
+	
 	//~ const double username = config["candle"].as<double>();
 	//~ std::cout << password << std::endl;
 	//~ std::cout << username << std::endl;
